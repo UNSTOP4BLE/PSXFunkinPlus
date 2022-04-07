bb45dd06992dfb958908724fe1ac9ce4898556d5
Lucky <baloghmarcellkrisztofer@gmail.com>
2022-04-07 20:58:16 +0200

    first upload

---
+++ src/audio_def.h
@@ -0,0 +1,53 @@
+#define XA_LENGTH(x) (((u64)(x) * 75) / 100 * IO_SECT_SIZE) //Centiseconds to sectors in bytes (w)
+
+typedef struct
+{
+	XA_File file;
+	u32 length;
+} XA_TrackDef;
+
+static const XA_TrackDef xa_tracks[] = {
+	//MENU.XA
+	{XA_Menu, XA_LENGTH(11295)}, //XA_GettinFreaky
+	{XA_Menu, XA_LENGTH(3840)},  //XA_GameOver
+	{XA_Menu, XA_LENGTH(408)},
+	//WEEK1A.XA
+	{XA_Week1A, XA_LENGTH(14401)}, //XA_Bopeebo
+	{XA_Week1A, XA_LENGTH(8000)}, //XA_Fresh
+	//WEEK1B.XA
+	{XA_Week1B, XA_LENGTH(8667)}, //XA_Dadbattle
+	{XA_Week1B, XA_LENGTH(6800)}, //XA_Tutorial
+	//WEEK1B.XA
+	{XA_TestWeek, XA_LENGTH(14401)}, //XA_Armageddon
+};
+
+static const char *xa_paths[] = {
+	"\\MUSIC\\MENU.XA;1",   //XA_Menu
+	"\\MUSIC\\WEEK1A.XA;1", //XA_Week1A
+	"\\MUSIC\\WEEK1B.XA;1", //XA_Week1B
+	"\\MUSIC\\TESTWEEK.XA;1", //XA_TestWeek
+	NULL,
+};
+
+typedef struct
+{
+	const char *name;
+	boolean vocal;
+} XA_Mp3;
+
+static const XA_Mp3 xa_mp3s[] = {
+	//MENU.XA
+	{"freaky", false},   //XA_GettinFreaky
+	{"gameover", false}, //XA_GameOver
+	{"fnf_loss_sfx", false}, //XA_GameOver
+	//WEEK1A.XA
+	{"bopeebo", true}, //XA_Bopeebo
+	{"fresh", true},   //XA_Fresh
+	//WEEK1B.XA
+	{"dadbattle", true}, //XA_Dadbattle
+	{"tutorial", false}, //XA_Tutorial
+	//TESTWEEK.XA
+	{"armageddon", false}, //XA_Armageddon
+	
+	{NULL, false}
+};
---
+++ src/stagedef_disc1.h
@@ -0,0 +1,80 @@
+	{ //StageId_1_1 (Bopeebo)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(1,1),FIXED_DEC(33,10)},
+		1, 1,
+		XA_Bopeebo, 0,
+		
+		StageId_1_2, STAGE_LOAD_FLAG
+	},
+	{ //StageId_1_2 (Fresh)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(13,10),FIXED_DEC(18,10)},
+		1, 2,
+		XA_Fresh, 2,
+		
+		StageId_1_3, STAGE_LOAD_FLAG
+	},
+	{ //StageId_1_3 (Dadbattle)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(13,10),FIXED_DEC(15,10),FIXED_DEC(23,10)},
+		1, 3,
+		XA_Dadbattle, 0,
+		
+		StageId_1_3, 0
+	},
+	{ //StageId_1_4 (Tutorial)
+		//Characters
+		{Char_BF_New, FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,  FIXED_DEC(0,1),  FIXED_DEC(-15,1)}, //TODO
+		{NULL,           FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(1,1),FIXED_DEC(1,1)},
+		1, 4,
+		XA_Tutorial, 2,
+		
+		StageId_1_4, 0
+	},
+	{ //StageId_1_5 (Bopeebo)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(1,1),FIXED_DEC(33,10)},
+		1, 5,
+		XA_Armageddon, 0,
+		
+		StageId_1_5, 0
+	}
