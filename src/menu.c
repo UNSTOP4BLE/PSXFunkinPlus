4f990da14916c9cb7bc5b3358aa1086834f20b2f
Lucky <baloghmarcellkrisztofer@gmail.com>
2022-05-02 13:22:50 +0200

    random shit
    and yes, ill bring back the gamemodes

--- src/menu.c
+++ src/menu.c
@@ -973,6 +973,7 @@ void Menu_Tick(void)
 					} spec_enum;
 				} spec;
 			} menu_options[] = {
+				{OptType_Enum,    "GAMEMODE", &stage.mode, {.spec_enum = {COUNT_OF(gamemode_strs), gamemode_strs}}},
 				{OptType_Boolean, "GHOST TAP ", &stage.ghost, {.spec_boolean = {0}}},
 				{OptType_Boolean, "DOWNSCROLL", &stage.downscroll, {.spec_boolean = {0}}},
 				{OptType_Boolean, "WIDESCREEN", &stage.widescreen, {.spec_boolean = {0}}}
