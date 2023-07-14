0a5bd17375aeedeceda1e427c3e77b1edd62c5ec
Lucky <baloghmarcellkrisztofer@gmail.com>
2023-07-14 17:36:45 +0200

    useless files

--- CEconfig/template_player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PSXF_GUARD_<CHARACTERNAME>_H
-#define PSXF_GUARD_<CHARACTERNAME>_H
-
-#include "../../scenes/stage/player.h"
-
-//<CharacterName> player functions
-Character *Char_<CharacterName>_New(fixed_t x, fixed_t y, fixed_t scale);
-
-#endif
